fed9772a004c0dec9fe7f39447e25f062a6a05dd
paige <88659700+paishee@users.noreply.github.com>
2024-04-10 12:39:23 -0500

    Update and rename lighting.server.lua to lighting.server.lua

--- Game/Workspace/lighting.server.lua
+++ Game/Workspace/lighting.server.lua
@@ -2,11 +2,16 @@ local lighting = game:GetService("Lighting");
 local screenGui = game:GetService("StarterGui").ScreenGui;
 local ingame = lighting.Ingame;
 
+
 lighting.ClockTime = 5;
-lighting.Atmosphere.Density = ingame.Density.Value;
 lighting.Brightness = ingame.Brihgtness.Value;
 lighting.Ambient = ingame.Ambient.Value;
 
+
+lighting.Atmosphere.Density = ingame.Density.Value;
+
+
 lighting.ColorCorrection.Contrast = ingame.Contrast.Value;
+
 
 screenGui.ngihtText.TextTransparency = 1;
